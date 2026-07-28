bb180ea20e35e926bd60175d46b8265b084bb08e
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-29 01:55:43 +0530

    added login api and functionality

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import { ROLES } from "../constant/model.constant.js";
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema(
   {
@@ -29,13 +30,17 @@ const userSchema = new Schema(
     },
     picture:{
       type:String,
-      default:"https://px.pixxo.io/test/user.png"
+      default:"https://px.pixxo.io/test/user.pnghttps://res.cloudinary.com/zld78yto/image/upload/v1783615661/134996379_da7ed7b0-5f66-4f97-a610-51100d3b9fd2_srspvw.jpg"
     }
   },
   {
     timestamps: true,
   },
 );
+
+userSchema.methods.comparePassword = async function(userPass){
+  return await bcrypt.compare(userPass,this.password);
+}
 
 let userModel = model("user", userSchema);
 
